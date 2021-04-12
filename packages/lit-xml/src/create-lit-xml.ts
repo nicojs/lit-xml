b26cc0f04316b0c2ed1f0d7cb7daf9c7694dd6ba
Hugo van Rijswijk <dev@hugovr.nl>
2021-04-12 13:19:54 +0200

    feat(create-lit-xml): add strictTemplateValues option

--- packages/lit-xml/src/create-lit-xml.ts
+++ packages/lit-xml/src/create-lit-xml.ts
@@ -5,11 +5,19 @@ const DEFAULT_OPTIONS: Readonly<LitXmlOptions> = {
   format: false,
   indent: 2,
   validators: [],
+  strictTemplateValues: false,
 };
 
-export type XmlTemplateLiteral = (xmlLiterals: TemplateStringsArray, ...values: unknown[]) => XmlFragment;
+export type XmlPrimitive = string | number | boolean | bigint | XmlFragment | XmlFragment[];
 
-export function createLitXml(overrideOptions?: Partial<LitXmlOptions>) {
+export type XmlTemplateLiteralTag<T> = (xmlLiterals: TemplateStringsArray, ...values: T[]) => XmlFragment;
+
+export type XmlTemplateLiteral = XmlTemplateLiteralTag<unknown>;
+export type StrictXmlTemplateLiteral = XmlTemplateLiteralTag<XmlPrimitive>;
+
+export function createLitXml<T extends Partial<LitXmlOptions>>(
+  overrideOptions?: T
+): XmlTemplateLiteralTag<T extends { strictTemplateValues: true } ? XmlPrimitive : unknown> {
   const options = Object.freeze({ ...DEFAULT_OPTIONS, ...overrideOptions });
   return function xml(xmlLiterals: TemplateStringsArray, ...values: unknown[]): XmlFragment {
     return new XmlFragment(xmlLiterals, values, options);
