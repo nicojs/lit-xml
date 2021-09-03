05003530e8d2934bcd8b750ed9517d81c07c0af1
Hugo van Rijswijk <git@hugovr.nl>
2021-09-03 16:30:39 +0200

    fix: export xml-fragment in root package

--- packages/lit-xml/src/index.ts
+++ packages/lit-xml/src/index.ts
@@ -2,4 +2,5 @@ import * as validators from './validators';
 export * from './create-lit-xml';
 export * from './invalid-xml-error';
 export * from './validator';
+export * from './xml-fragment';
 export { validators };
